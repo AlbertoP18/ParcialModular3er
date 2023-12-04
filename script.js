ecf58c25272407f00dbe80ef70efd231cd25b54d
AlbertoP18 <alberto.perez9@misena.edu.co>
2023-12-03 20:34:06 -0500

    frontend final

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-    // URL del archivo JSON en la nube
-    const jsonUrl = 'notas.json';
+    // // URL del archivo JSON en la nube
+    // const jsonUrl = 'https://albertop18.github.io/api-Notas/notas.json';
 
-    // Obtener datos JSON mediante fetch
-    fetch(jsonUrl)
-      .then(response => response.json())
-      .then(data => {
-        // Obtener la tabla y el cuerpo de la tabla
-        const table = document.getElementById('miTabla');
-        const tbody = table.querySelector('tbody');
+    // // Obtener datos JSON mediante fetch
+    // fetch('jsonUrl')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data)
+    //     // Obtener la tabla y el cuerpo de la tabla
+    //     const table = document.getElementById('miTabla');
+    //     const tbody = table.querySelector('tbody');
 
-        // Limpiar el cuerpo de la tabla
-        tbody.innerHTML = '';
+    //     // Limpiar el cuerpo de la tabla
+    //     tbody.innerHTML = '';
 
-        // Iterar sobre los datos del JSON y agregar filas a la tabla
-        data.forEach(rowData => {
-          const row = document.createElement('tr');
+    //     // Iterar sobre los datos del JSON y agregar filas a la tabla
+    //     data.forEach(rowData => {
+    //       const row = document.createElement('tr');
 
-          Object.values(rowData).forEach(value => {
-            const cell = document.createElement('td');
-            cell.textContent = value;
-            row.appendChild(cell);
-          });
+    //       Object.values(rowData).forEach(value => {
+    //         const cell = document.createElement('td');
+    //         cell.textContent = value;
+    //         row.appendChild(cell);
+    //       });
 
-          tbody.appendChild(row);
-        });
-      })
-      .catch(error => console.error('Error al obtener datos JSON:', error));
+    //       tbody.appendChild(row);
+    //     });
+    //   })
+    //   .catch(error => console.error('Error al obtener datos JSON:', error));
